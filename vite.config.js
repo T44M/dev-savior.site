c9ca350b69bd3a3fef89842643947edf2e5402d9
TM_GO <go.matsumiya@gmail.com>
2025-01-30 23:22:37 +0900

    change

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dev-savior.site/',  // リポジトリ名に合わせて変更
+  base: '/',
+  // base: '/dev-savior.site/',  // リポジトリ名に合わせて変更
 })
